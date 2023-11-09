342d5227a99ebb3e23078213f8c970b891fc8d7e
~Achal <106076516+Achal-00@users.noreply.github.com>
2023-11-10 02:37:57 +0530

    add not found page (#30)

---
+++ src/app/not-found.js
@@ -0,0 +1,27 @@
+import "@/styles/components/not-found.scss";
+import Link from "next/link";
+
+export default function notFound() {
+  return (
+    <section className="not-found-container">
+      <div className="not-found-content">
+        <h1>Oops!</h1>
+        <h4>404 Page Not Found</h4>
+        <p>
+          Looks like this page hasn't been developed yet.
+          <br />
+          Would you like to join and be a part of creating it?
+          <br />
+          <Link
+            href="https://github.com/code4sac/opensac.org"
+            className="not-found-link"
+            target="_blank"
+          >
+            Get Started
+          </Link>
+        </p>
+        <p>Explore our site using the menu above.</p>
+      </div>
+    </section>
+  );
+}
