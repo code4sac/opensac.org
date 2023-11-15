70dcdf05dca89ca74408c027b03f63c43d9dde1c
Automated <hello@opensac.org>
2023-11-15 11:30:33 +0000

    Deploying to gh-pages from @ code4sac/opensac.org@3caaaa8dc533feaaed03ede00feb8e2c11750240 🚀

--- _next/static/chunks/app/faq/page-1fcbc61fe62d58d7.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[799],{3571:function(e,n,t){Promise.resolve().then(t.t.bind(t,8326,23)),Promise.resolve().then(t.bind(t,8836)),Promise.resolve().then(t.t.bind(t,2573,23))},8836:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return FaqAccordion}});var r=t(7437),o=t(2265);function FaqAccordion(e){let[n,t]=(0,o.useState)(!1);return(0,r.jsxs)("section",{className:n?"faq-question question-open":"faq-question",children:[(0,r.jsx)("a",{onClick:()=>{t(!n)},children:(0,r.jsx)("p",{className:"faq-question-semiheader",children:e.title})}),(0,r.jsx)("p",{className:"question-text",children:e.text})]},e.key)}},2573:function(){},622:function(e,n,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var r=t(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),i=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,n,t){var r,u={},f=null,a=null;for(r in void 0!==t&&(f=""+t),void 0!==n.key&&(f=""+n.key),void 0!==n.ref&&(a=n.ref),n)s.call(n,r)&&!c.hasOwnProperty(r)&&(u[r]=n[r]);if(e&&e.defaultProps)for(r in n=e.defaultProps)void 0===u[r]&&(u[r]=n[r]);return{$$typeof:o,type:e,key:f,ref:a,props:u,_owner:i.current}}n.jsx=q,n.jsxs=q},7437:function(e,n,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[326,971,472,744],function(){return e(e.s=3571)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/not-found-6dab70ddc40f0219.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{7304:function(n,e,t){Promise.resolve().then(t.t.bind(t,8326,23)),Promise.resolve().then(t.t.bind(t,6494,23))},6494:function(){}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=7304)}),_N_E=n.O()}]);
