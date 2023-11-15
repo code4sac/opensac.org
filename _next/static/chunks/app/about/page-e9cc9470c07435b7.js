dc62fa267f70b6feccfc0dfd4d2ab3e4568439b3
Automated <hello@opensac.org>
2023-11-15 01:07:56 +0000

    Deploying to gh-pages from @ code4sac/opensac.org@1f4f46c21890112842b50db5b61f602545f9f19f 🚀

---
+++ _next/static/chunks/app/about/page-e9cc9470c07435b7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{5822:function(n,e,u){Promise.resolve().then(u.t.bind(u,1305,23))},1305:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=5822)}),_N_E=n.O()}]);
