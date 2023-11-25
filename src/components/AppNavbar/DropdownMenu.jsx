44969c101d2a8d67a5772741b3c7b71affd71aa1
nateb <nw3455@gmail.com>
2023-11-25 01:04:45 -0800

    Improve navbar and delete figma generated code.

---
+++ src/components/AppNavbar/DropdownMenu.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import '@/styles/components/dropdown-menu.scss'
+import Link from 'next/link'
+
+/**
+ * Dropdown menu for navbar.
+ * @returns {JSX.Element}
+ */
+const DropdownMenu = () => {
+  return (
+    <div className="dropdown-menu">
+      <div className="section-item">
+        <div className="text-wrapper-2">Get Started</div>
+        <p className="section-content">
+          Get started today and volunteer with us to drive positive change through technology, making a meaningful
+          impact in your community!
+        </p>
+        <Link href="" className="dropdown-button">Get Started</Link>
+      </div>
+      <div className="section-item">
+        <div className="text-wrapper-2">Project</div>
+        <p className="section-content">
+          Explore our projects and discover how you can contribute your skills to drive innovation and create positive
+          change.
+        </p>
+        <Link href="" className="dropdown-button">See Projects</Link>
+      </div>
+      <div className="section-item">
+        <div className="text-wrapper-2">Pitch a Project</div>
+        <p className="section-content">
+          Get the latest information and guidance for anyone who want to propose new projects or ideas to the Open
+          Sacramento community.
+        </p>
+        <Link href="" className="dropdown-button">Pitch a Project</Link>
+      </div>
+      <div className="section-item">
+        <div className="text-wrapper-2">Community</div>
+        <p className="section-content-2">
+          Join us for our weekly meetings on <Link href="#" className="text-wrapper-3">meetup</Link>.
+        </p>
+        <p className="section-content-2">
+          For any questions, visit our <Link href="/faq" className="text-wrapper-3">FAQs</Link>.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DropdownMenu
---
+++ src/components/AppNavbar/index.js
@@ -0,0 +1,4 @@
+import AppNavbar from './AppNavbar'
+import '@/styles/components/navigation-bar.scss'
+
+export default AppNavbar
