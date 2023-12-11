9138819e09c6ce92e97b59c0069dda1620a9cf23
natebass <nw3455@gmail.com>
2023-12-11 03:16:42 -0800

    Update partnership section to match Figma design.

--- src/app/home/homeSectionPartnerships.jsx
+++ src/app/home/homeSectionPartnerships.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 /**
  * Home Partnerships Section
  * @returns {JSX.Element}
@@ -6,39 +7,40 @@ import Link from "next/link";
 
 export default function HomeSectionPartnership({sectionType}) {
     return (
-    <section className={`home-section-${sectionType} partnership-section`}>
-    <div className={`home-section-${sectionType}-container`}>
-    <div className={'home-section-heading-underline-partnership'}>
-    <h1 className={`heading-large-section-sub`}>Our Collaborations and Partnerships</h1>
-    </div>
-    <div className={`partnership-section-paragraph-container`}>
-    <p className={`home-numbered-paragraph-partnerships`}>We have had the privilege of working with a diverse array of companies, organizations, and government entities, some of which are showcased below.</p>
-   <div className={`home-projects-section-partnerships`}>
-   <div>
-   <Link href={`https://www.cityofsacramento.gov/`}>
-   <img src="/img/partnership-icons/cityofsacramento.png" 
-   width="300px"
-   height="120.83px" 
-   color="white"
-   alt="cityofsacramento"
-   />
-   </Link>
-   </div>
-   <div>
-   <Link href={`http://www.dbaarts.org/`}>
-   <img src="/img/partnership-icons/DBA.png"
-   width="180.57px"
-   height="130.83px"
-   alt="DBA"
-   />
-   </Link>
-   </div>
-   <div>
-  <Link href={`https://codeforamerica.org/`}><img src="/img/partnership-icons/codeforamerica.png" alt="Code for America" /></Link>
-   </div>
-   </div>
-    </div>
-    </div>
-    </section>
+      <section className={`home-section-${sectionType} partnership-section`}>
+        <div className={`home-section-${sectionType}-container`}>
+          <div className={'home-section-heading-underline-partnership'}>
+            <h1 className={`heading-large-section-sub`}>Our Collaborations and Partnerships</h1>
+          </div>
+          <div className={`partnership-section-paragraph-container`}>
+            <p className={`home-numbered-paragraph-partnerships`}>We have had the privilege of working with a
+              diverse array of companies, organizations, and government entities, some of which are showcased
+              below.</p>
+            <div className={`home-projects-section-partnerships`}>
+              <Link href={`https://www.cityofsacramento.gov/`} style={{paddingRight: 26}}>
+                <img src="/img/partnership-icons/cityofsacramento.png"
+                     width="300"
+                     height="73"
+                     color="white"
+                     alt="cityofsacramento"
+                />
+              </Link>
+              <Link href={`http://www.dbaarts.org/`}>
+                <img src="/img/partnership-icons/DBA.png"
+                     width="137"
+                     height="137"
+                     alt="DBA"
+                />
+              </Link>
+              <Link href={`https://codeforamerica.org/`}>
+                <img src="/img/partnership-icons/logo_openfresno_white_transparent.png"
+                     width="233"
+                     height="112"
+                     alt="DBA"/>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     );
 }
