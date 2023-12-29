ad359f336c945f6b28be921bd49fa88582756993
nateb <nw3455@gmail.com>
2023-12-29 08:08:11 -0800

    Update navbar desktop layout. No mobile layout.

--- src/components/AppNavbar/AppNavbar.jsx
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import {usePathname} from 'next/navigation'
 import DropdownMenu from './DropdownMenu'
 
 /**
@@ -15,65 +15,58 @@ function setOpacity(currentScrollHeight) {
 }
 
 /**
- * Default application navigation bar.
- * @param {boolean} [extended=false] Fade on scroll.
- * @param {fadeLayout} [fadeLayout=false] Fade on scroll.
+ * Set the navbar to fade on scroll.
+ * Note: This method, its arguments, and the fact that you need to return it in use effect, is confusing and could be refactored. It is extracted as a function so that it can be reused if any other routes need a fading navbar.
+ * @param setFadeLayout
+ * @param setCurrentScrollHeight
+ * @returns {function(): void} Make sure to return a function removing the event listener in use effect. I don't know why.
+ */
+function registerNavbarFadeLayout(setFadeLayout, setCurrentScrollHeight) {
+  setFadeLayout(true)
+  const onScroll = () => {
+    setCurrentScrollHeight(window.scrollY)
+  }
+  window.removeEventListener('scroll', onScroll)
+  window.addEventListener('scroll', onScroll, {passive: true})
+  return () => window.removeEventListener('scroll', onScroll)
+}
+
+/**
+ * Default application navigation bar. Links animate on page change. Extended menu that shows more text underneath the navbar.
+ * - Desktop: Multiple navigation links in the center. Link on the right side to show extended menu.
+ * - Mobile: All menu items are in collapsable menu.
  * @returns {JSX.Element}
  */
-export default function AppNavbar({ extended = false, fade = false }) {
+export default function AppNavbar() {
   const [extendedMenuVisible, showExtendedMenu] = useState(false)
-  const [previousURL, setPreviousURL] = useState('/')
-  const [fadeLayout, setFadeLayout] = useState(fade)
-  const [currentScrollHeight, setCurrentScrollHeight] = useState(9999)
-  const onClickExtendMenu = () => showExtendedMenu(!extendedMenuVisible)
+  const [fadeLayout, setFadeLayout] = useState(false)
+  const [currentScrollHeight, setCurrentScrollHeight] = useState(0)
   const backgroundOpacity = setOpacity(currentScrollHeight)
   const websiteURL = usePathname()
-
-  if (extendedMenuVisible == true && fadeLayout == true) {
-    setFadeLayout(false)
-  } else {
+  useEffect(() => {
+    showExtendedMenu(false)
+    if (currentScrollHeight === 0) setCurrentScrollHeight(window.scrollY)
     switch (websiteURL) {
       case '/':
-        if (fadeLayout == false && extendedMenuVisible == false) {
-          setFadeLayout(true)
-        }
-        break
+        return registerNavbarFadeLayout(setFadeLayout, setCurrentScrollHeight)
       default:
-        if (fadeLayout == true && extendedMenuVisible == false) setFadeLayout(false)
+        setFadeLayout(false)
     }
-  }
-
-  if (websiteURL !== previousURL) {
-    showExtendedMenu(false)
-    setPreviousURL(websiteURL)
-  }
-
-  useEffect(() => {
-    if (currentScrollHeight == 9999) setCurrentScrollHeight(window.scrollY)
-    if (fadeLayout) {
-      const onScroll = () => { setCurrentScrollHeight(window.scrollY) }
-      window.removeEventListener('scroll', onScroll)
-      window.addEventListener('scroll', onScroll, { passive: true })
-      return () => window.removeEventListener('scroll', onScroll)
-    } else {
-
-    }
-  }, [])
-
+  }, [websiteURL])
   return (
     <div className='navbar-background'>
-      <nav className={`navbar-toolbar`} style={fadeLayout ? { backgroundColor: `rgba(0,0,0, ${backgroundOpacity})` } : { backgroundColor: 'inherit' }}>
+      <nav className={`navbar-toolbar`} style={fadeLayout && !extendedMenuVisible ? { backgroundColor: `rgba(0,0,0, ${backgroundOpacity})` } : { backgroundColor: 'inherit' }}>
         <div className={'navbar-toolbar-main'}>
-          <div className={'navbar-toolbar-content'}>
+          <div className={'navbar-toolbar-content container-xxl'}>
             <div className={'navbar-left-container'}>
               <Link className={'navbar-left-section'} href="/">
-                <img src="/img/opensac_logo_dark_01.png" alt="Open Sacramento logo" height="60" width="200" />
+                <img src="/img/logo_opensac_black_transparent_2.png" alt="Open Sacramento logo" height="60" width="220" />
               </Link>
             </div>
 
             <div className={'navbar-middle-container'}>
-              < ul className={'navbar-middle-section'}>
-                < li className={`navbar-link ${websiteURL === '/' ? 'navbar-link-selected one' : ''}`}>
+              <ul className={'navbar-middle-section'}>
+                <li className={`navbar-link ${websiteURL === '/' ? 'navbar-link-selected one' : ''}`}>
                   <Link href="/">Home</Link>
                 </li>
                 <li className={`navbar-link ${websiteURL === '/about' ? 'navbar-link-selected two' : ''}`}>
@@ -90,11 +83,11 @@ export default function AppNavbar({ extended = false, fade = false }) {
             </div>
 
             <div className={`navbar-right-container ${extendedMenuVisible ? 'navbar-extend-background-active' : 'navbar-extend-background-inactive'}`}>
-              <div className={`navbar-right-section`}>
+              <div className={`navbar-right-section`} onClick={() => {showExtendedMenu(!extendedMenuVisible)}}>
                 <div className={`navbar-nested-parent-link`}>
-                  <button className={`${extendedMenuVisible ? 'navbar-extended-button-extended' : 'navbar-extended-button'}`} type="button" alt="Extend navigation bar." onClick={onClickExtendMenu}>
-                    <span className="navbar-extend-button-text">Get Involved</span>
-                  </button>
+                  <div className={`${extendedMenuVisible ? 'navbar-toggle-extend-button-extended' : 'navbar-toggle-extend-button'}`} type="button" alt="Extend navigation bar.">
+                    <span className={`navbar-extend-button-text ${extendedMenuVisible ? 'navbar-extend-button-text-extended' : null}`}>Get Involved</span>
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/AppNavbar/DropdownMenu.jsx
+++ src/components/AppNavbar/DropdownMenu.jsx
@@ -10,40 +10,44 @@ const DropdownMenu = (visible = false) => {
   function hide() {
     visible = false
   }
+
   return (
-    <div className="dropdown-menu" style={visible ? {} : { display: 'none' }}>
-      <div className="section-item">
-        <div className="text-wrapper-2">Get Started</div>
-        <p className="section-content">
-          Get started today and volunteer with us to drive positive change through technology, making a meaningful
-          impact in your community!
-        </p>
-        <Link href="get-started" className="dropdown-button" onClick={hide}>Get Started</Link>
+    <div className="dropdown-menu" style={visible ? {} : {display: 'none'}}>
+      <div className={'navbar-extended-mobile-container'}>
       </div>
-      <div className="section-item">
-        <div className="text-wrapper-2">Projects</div>
-        <p className="section-content">
-          Explore our projects and discover how you can contribute your skills to drive innovation and create positive
-          change.
-        </p>
-        <Link href="projects" className="dropdown-button">See Projects</Link>
-      </div>
-      <div className="section-item">
-        <div className="text-wrapper-2">Pitch a Project</div>
-        <p className="section-content">
-          Get the latest information and guidance for anyone who want to propose new projects or ideas to the Open
-          Sacramento community.
-        </p>
-        <Link href="pitch" className="dropdown-button">Pitch a Project</Link>
-      </div>
-      <div className="section-item">
-        <div className="text-wrapper-2">Community</div>
-        <p className="section-content-2">
-          Join us for our weekly meetings on <Link href="#" className="text-wrapper-3">meetup</Link>.
-        </p>
-        <p className="section-content-2">
-          For any questions, visit our <Link href="/faq" className="text-wrapper-3">FAQs</Link>.
-        </p>
+      <div className={'navbar-extended-desktop-container container-xxl'}>
+        <div className="navbar-extended-desktop-section-item">
+          <div className="navbar-extended-text-wrapper-2">Get Started</div>
+          <p className="navbar-extended-desktop-section-content">
+            Get started today and volunteer with us to drive positive change through technology, making a meaningful
+            impact in your community!
+          </p>
+          <Link href="get-started" className="navbar-extended-button" onClick={hide}>Get Started</Link>
+        </div>
+        <div className="navbar-extended-desktop-section-item">
+          <div className="navbar-extended-text-wrapper-2">Projects</div>
+          <p className="navbar-extended-desktop-section-content">
+            Explore our projects and discover how you can contribute your skills to drive innovation and create positive
+            change.
+          </p>
+          <Link href="projects" className="navbar-extended-button">See Projects</Link>
+        </div>
+        <div className="navbar-extended-desktop-section-item">
+          <div className="navbar-extended-text-wrapper-2">Pitch a Project</div>
+          <p className="navbar-extended-desktop-section-content">
+            Get the latest information and guidance for anyone who want to propose new projects or ideas to the Open
+            Sacramento community.
+          </p>
+          <Link href="pitch" className="navbar-extended-button">Pitch a Project</Link>
+        </div>
+        <div className="navbar-extended-desktop-section-item">
+          <p className="section-content-2">
+            Join us for our weekly meetings on <Link href="#" className="text-wrapper-3">meetup</Link>.
+          </p>
+          <p className="section-content-2">
+            For any questions, visit our <Link href="/faq" className="text-wrapper-3">FAQs</Link>.
+          </p>
+        </div>
       </div>
     </div>
   )
