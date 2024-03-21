8c51b954c594d50059ca7f3732ddc16458a6661a
Dan Fey <feydanm@gmail.com>
2024-03-20 21:24:04 -0700

    adding comment to generateStaticParams function

--- src/app/projects/[project]/page.js
+++ src/app/projects/[project]/page.js
@@ -4,6 +4,7 @@ import "@/styles/pages/faq.scss";
 import "@/styles/pages/projects.scss";
 import SingleProject from "./singleProject";
 
+// See https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 export async function generateStaticParams() {
   const ghResponses = await fetch(
     `https://api.github.com/orgs/${githubOwner}/repos?per_page=20&sort=updated&direction=desc`
