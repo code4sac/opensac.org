485b5da74f89a8fe64ba429c170afe169f58fd82
Nate <nw3455@gmail.com>
2024-03-30 16:40:44 -0700

    Remove commented out css import.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,6 @@
 import { githubOwner } from "@/constants";
 import "@/styles/pages/projects.scss";
 import Projects from "./projects";
-// import '@/styles/pages/faq.scss'
 
 export default function Page() {
   return (
