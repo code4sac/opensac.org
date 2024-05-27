62f42213fcb30bd6c28bcd3df398995acb885bc6
lisin <lisinnic@gmail.com>
2024-05-26 17:13:07 -0700

    Fix punctuation and formatting issues

---
+++ src/components/AppFooter.js
@@ -0,0 +1,106 @@
+import Link from "next/link";
+
+/**
+ * Default application footer.
+ * @returns {JSX.Element}
+ */
+export default function AppFooter() {
+  return (
+    <footer>
+      <div className="footerContainerTop">
+        <div className="footerMessage">
+          <h2 className="footerMessageHeader">Get Involved.</h2>
+          <p className="footerMessageParagraph">
+            Open Sacramento is a tax-deductible civic-tech community dedicated
+            to using technology and open data for positive civic change in
+            Sacramento.
+          </p>
+        </div>
+
+        <div className="footerActionLinks">
+          <p className="footerActionLinksItem">
+            Join us for our weekly meetings on{" "}
+            <a
+              className="footerActionLinksAnchor"
+              href="https://www.meetup.com/code4sac/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Link to Meetup."
+            >
+              meetup
+            </a>
+            .
+          </p>
+          <p className="footerActionLinksItem">
+            See our{" "}
+            <Link href="/projects" className="footerActionLinksAnchor">
+              projects
+            </Link>
+            .
+          </p>
+          <p className="footerActionLinksItem">
+            For any questions, visit our{" "}
+            <Link href="/faq" className="footerActionLinksAnchor">
+              FAQs
+            </Link>
+            .
+          </p>
+        </div>
+
+        <div className="footerSocialMedia">
+          <Link
+            href="https://www.facebook.com/code4sac"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footerSocialMediaLink"
+          >
+            <img
+              src="/img/socialmedia/facebook.svg"
+              alt="Facebook"
+              className="footerSocialMediaIcon"
+            />
+          </Link>
+          <Link
+            href="https://twitter.com/code4sac"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footerSocialMediaLink"
+          >
+            <img
+              src="/img/socialmedia/twitter.svg"
+              alt="X"
+              className="footerSocialMediaIcon"
+            />
+          </Link>
+          <Link
+            href="https://www.youtube.com/@code4sac"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footerSocialMediaLink"
+          >
+            <img
+              src="/img/socialmedia/youtube.svg"
+              alt="Youtube"
+              className="footerSocialMediaIcon"
+            />
+          </Link>
+        </div>
+      </div>
+
+      <div className="footerBottomContainer">
+        <div className="footerBottomTitle">Open Sacramento</div>
+        <div className="footerBottomLinksContainer">
+          <Link href="/get-started" className="footerBottomLink">
+            get started
+          </Link>
+          <Link href="/about/#team" className="footerBottomLink">
+            our team
+          </Link>
+          <Link href="/code-of-conduct" className="footerBottomLink">
+            code of conduct
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
