76c792575ed65718900c9a00f64048999beb0c28
Dan Fey <feydanm@gmail.com>
2024-05-30 08:56:57 -0700

    uUpdating code of conduct page to use conflict reporting form

--- src/app/code-of-conduct/conductSectionPolicies.jsx
+++ src/app/code-of-conduct/conductSectionPolicies.jsx
@@ -2,22 +2,31 @@ export default function conductSectionPolicies() {
   return (
     <section className={`conduct-dark-theme`}>
       <div className={`conduct-policy-landing-container conduct-dark-theme`}>
-        <div className={`conduct-section-heading-container conduct-heading-underline underline-alt`}>
-          <h1 className={`general-heading-section general-heading-section-alt`}>anti-harassment policy</h1>
+        <div
+          className={`conduct-section-heading-container conduct-heading-underline underline-alt`}
+        >
+          <h1 className={`general-heading-section general-heading-section-alt`}>
+            anti-harassment policy
+          </h1>
           <h2 className={`conduct-sub-h1-heading`}>
             Promoting a Safe and Respectful Community
           </h2>
         </div>
         <p className={`conduct-item-paragraph`}>
-          This anti-harassment policy is based on <a className={`conduct-paragraph-anchor`} href="/">the example
-          policy</a> from
-          the Geek Feminism wiki, created by the Ada Initiative and other volunteers.
-          <br/><br/>
-          This policy is based on several other policies, including the Ohio LinuxFest anti-harassment policy,
-          written by Esther Filderman and Beth Lynn Eicher, and the Con Anti-Harassment Project.
-          Mary Gardiner, Valerie Aurora, Sarah Smith, and Donna Benjamin generalized the policies
-          and added supporting material. Many members of LinuxChix, Geek Feminism and other groups contributed to this
-          work.
+          This anti-harassment policy is based on{" "}
+          <a className={`conduct-paragraph-anchor`} href="/">
+            the example policy
+          </a>{" "}
+          from the Geek Feminism wiki, created by the Ada Initiative and other
+          volunteers.
+          <br />
+          <br />
+          This policy is based on several other policies, including the Ohio
+          LinuxFest anti-harassment policy, written by Esther Filderman and Beth
+          Lynn Eicher, and the Con Anti-Harassment Project. Mary Gardiner,
+          Valerie Aurora, Sarah Smith, and Donna Benjamin generalized the
+          policies and added supporting material. Many members of LinuxChix,
+          Geek Feminism and other groups contributed to this work.
         </p>
       </div>
 
@@ -27,72 +36,62 @@ export default function conductSectionPolicies() {
         </h3>
 
         <p className={`conduct-item-paragraph`}>
-          In addition to governing our own events by this policy, Open Sacramento will only lend its brand and fund
-          groups
-          that offer an anti-harassment policy to their attendees. For information on how to offer an anti-harassment
-          policy
-          to your group, <a className={`conduct-paragraph-anchor`} href="/">see this guide.</a>
+          In addition to governing our own events by this policy, Open
+          Sacramento will only lend its brand and fund groups that offer an
+          anti-harassment policy to their attendees. For information on how to
+          offer an anti-harassment policy to your group,{" "}
+          <a className={`conduct-paragraph-anchor`} href="/">
+            see this guide.
+          </a>
         </p>
 
         <p className={`conduct-item-paragraph`}>
-          Open Sacramento is dedicated to providing a harassment-free experience for everyone regardless of gender,
-          gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, or
-          religion.
-          We do not tolerate harassment of staff, presenters, and participants in any form. Sexual language and imagery
-          is not appropriate for any Open Sacramento event or network activity, including talks.
-          Anyone in violation of these policies may expelled from Open Sacramento network activities, events, and
-          digital forums,
+          Open Sacramento is dedicated to providing a harassment-free experience
+          for everyone regardless of gender, gender identity and expression,
+          sexual orientation, disability, physical appearance, body size, race,
+          age, or religion. We do not tolerate harassment of staff, presenters,
+          and participants in any form. Sexual language and imagery is not
+          appropriate for any Open Sacramento event or network activity,
+          including talks. Anyone in violation of these policies may expelled
+          from Open Sacramento network activities, events, and digital forums,
           at the discretion of the event organizer or forum administrator.
         </p>
 
         <p className={`conduct-item-paragraph`}>
-          Harassment includes but is not limited to: offensive verbal or written comments related to gender, gender
-          identity
-          and expression, sexual orientation, disability, physical appearance, body size, race, religion; sexual images
-          in public spaces; deliberate intimidation; stalking; following; harassing photography or recording;
-          sustained disruption of talks or other events; inappropriate physical contact; unwelcome sexual attention;
-          unwarranted exclusion; and patronizing language or action.
+          Harassment includes but is not limited to: offensive verbal or written
+          comments related to gender, gender identity and expression, sexual
+          orientation, disability, physical appearance, body size, race,
+          religion; sexual images in public spaces; deliberate intimidation;
+          stalking; following; harassing photography or recording; sustained
+          disruption of talks or other events; inappropriate physical contact;
+          unwelcome sexual attention; unwarranted exclusion; and patronizing
+          language or action.
         </p>
 
         <p className={`conduct-item-paragraph`}>
-          If a participant engages in harassing behavior, the organizers may take any action they deem appropriate,
-          including warning the offender or expulsion from Open Sacramento network activities, events, and digital
-          forums.
+          If a participant engages in harassing behavior, the organizers may
+          take any action they deem appropriate, including warning the offender
+          or expulsion from Open Sacramento network activities, events, and
+          digital forums.
         </p>
 
         <p className={`conduct-item-paragraph`}>
-          If you are being harassed, notice that someone else is being harassed, or have any other concerns,
-          please contact a member of the event staff or forum administrator immediately. You can contact them at leaders
-          [at]
-          opensacramento [dot] org or (916) 835-9607. Event staff or forum administrators will be happy to help
-          participants
-          contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing
-          harassment
-          to feel safe for the duration of the event.
+          If you are being harassed, notice that someone else is being harassed,
+          or have any other concerns, please fill out{" "}
+          <a href="https://forms.gle/JF3Q1AQA6pGxNSNS8">this form</a>.
         </p>
 
         <p className={`conduct-item-paragraph`}>
-          If you cannot reach an event organizer or forum administrator and/or it is an emergency, please call 911
-          and/or
-          remove yourself from the situation.
-        </p>
-
-        <p className={`conduct-item-paragraph`}>
-          You can also contact Open Sacramento about harassment at hello@codeforsacramento.org and feel free to use the
-          email template below. Open Sacramento staff acknowledge that we are not always in a position to evaluate a
-          given situation
-          due to the number of events and the fact that our team is not always present. However, we are hopeful that by
-          providing
-          these guidelines we are establishing a community that jointly adheres to these values and can provide an
-          environment
-          that is welcoming to all.
+          We are hopeful that by providing these guidelines we are establishing
+          a community that jointly adheres to these values and can provide an
+          environment that is welcoming to all.
         </p>
 
         <h3 className={`conduct-sub-heading`}>
-          We value your attendance and hope that by communicating these expectations widely we can all enjoy
-          a harassment-free environment.
+          We value your attendance and hope that by communicating these
+          expectations widely we can all enjoy a harassment-free environment.
         </h3>
       </div>
     </section>
-  )
+  );
 }
