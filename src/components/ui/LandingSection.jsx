1a3ad5de1ac4613fbbdf3bfb093ab628c8a788d8
nateb <nw3455@gmail.com>
2024-07-10 16:35:51 -0700

    Finish get started and pitch page steps component.

--- src/components/ui/LandingSection.jsx
+++ src/components/ui/LandingSection.jsx
@@ -1,3 +1,9 @@
+/**
+ * Page section with 50/50 split.
+ * @param children
+ * @param sectionType
+ * @returns {JSX.Element}
+ */
 export default function LandingSection({children, sectionType}) {
   return (
     <section className={`section-${sectionType} split-section-container`}>
