dd942427fbf4ffc462b14468213f0e97f8a59489
nateb <nw3455@gmail.com>
2024-07-10 16:35:51 -0700

    Finish get started and pitch page steps component.

--- src/app/pitch/page.js
+++ src/app/pitch/page.js
@@ -1,6 +1,8 @@
-  import LandingSection from "@/components/ui/LandingSection";
+import LandingSection from "@/components/ui/LandingSection";
 import HeaderUnderline from "@/components/content/HeaderUnderline";
-import PitchSectionInstructions from "@/app/pitch/pitchSectionInstructions";
+import instructionData from "./instructions.json";
+import SidebarSection from "@/components/ui/SidebarSection";
+import Steps from "@/components/content/Steps";
 
 /**
  * Section type. Displays light or dark themes.
@@ -27,7 +29,16 @@ export default function PitchPage() {
           Pitch your project to make a positive impact on Sacramento using technology and civic engagement.
         </div>
       </LandingSection>
-      <PitchSectionInstructions sectionType={SectionType.dark}/>
+      <SidebarSection sectionType={SectionType.dark}>
+        <div className={`sidebar-info-container`}>
+          <HeaderUnderline sectionType={SectionType.dark}
+                           title="Pitching a project" description="How to Start a New Project">
+            Ready to turn your civic tech idea into reality? Follow these simple steps to pitch your project idea to
+            Open Sacramento.
+          </HeaderUnderline>
+        </div>
+        <Steps sectionType={SectionType.dark} steps={instructionData}></Steps>
+      </SidebarSection>
     </div>
   )
 }
--- src/components/content/HeaderUnderline.jsx
+++ src/components/content/HeaderUnderline.jsx
@@ -6,11 +6,16 @@ import {SectionType} from "@/app/home/home";
  */
 export default function HeaderUnderline({children, className, sectionType, title, description, large}) {
   return (
-    <div className={`${className} underline-header-container heading-underline ${sectionType === SectionType.dark && 'underline-alt'}`}>
-      <h1
-        className={`general-heading-section ${sectionType === SectionType.dark && 'general-heading-section-alt'}`}>{title}</h1>
-      <h2 className={`${large ? 'sub-heading-large' : 'sub-heading'}`}>{description}</h2>
-      {children}
-    </div>
+    <>
+      <div
+        className={`${className} underline-header-container heading-underline ${sectionType === SectionType.dark && 'underline-alt'}`}>
+        <h1
+          className={`general-heading-section ${sectionType === SectionType.dark && 'general-heading-section-alt'}`}>{title}</h1>
+        <h2 className={`${large ? 'sub-heading-large' : 'sub-heading'}`}>{description}</h2>
+      </div>
+      {children && <p className={`heading-paragraph-large`}>
+        {children}
+      </p>}
+    </>
   )
 }
