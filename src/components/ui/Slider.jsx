d7427123db35dff7a442a1d62f2cc9ac9bfafd17
Brian Sims <briansims623@gmail.com>
2024-07-24 17:06:31 -0700

    Created Slider component and added it to single project page screenshots section mobile version to match figma design.

---
+++ src/components/ui/Slider.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react'; // import from 'keen-slider/react.es' for to get an ES module
+
+export default function Slider ({containerClass, children}) {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [loaded, setLoaded] = useState(false);
+    const [sliderRef, instanceRef] = useKeenSlider({
+        initial: 0,
+        slideChanged(slider) {
+          setCurrentSlide(slider.track.details.rel)
+        },
+        created() {
+          setLoaded(true)
+        },
+      });
+
+  return (
+    <div className={containerClass}>
+        <div ref={sliderRef} className="keen-slider">
+            {children}
+        </div>
+        {loaded && instanceRef.current && (
+          <div className="dots">
+            {[
+              ...Array(instanceRef.current.track.details.slides.length).keys(),
+            ].map((idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx)
+                  }}
+                  className={"dot" + (currentSlide === idx ? " active" : "")}
+                ></button>
+              )
+            })}
+          </div>
+        )}
+    </div>
+  )
+}
